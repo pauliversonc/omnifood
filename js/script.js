28fecd2396713085493ff6ab2923df80e7a375a6
Paul Iverson Cortez <pauliversonc@gmail.com>
2023-03-10 13:45:28 +0800

    fix icons

--- js/script.js
+++ js/script.js
@@ -1,9 +1,6 @@
 const navBtn = document.querySelector('.btn-mobile-nav');
 const navElem = document.querySelector('.header');
 
-console.log(navBtn)
-console.log(navElem)
-
 navBtn.addEventListener("click", function() {
   navElem.classList.toggle("nav-open")
 });
